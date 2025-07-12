64c6f5b3ed02cdf0b3bed77d0250a3722704a709
nie <22196078-7@qq.com>
2025-07-12 14:54:26 +0800

    增加归并排序与堆排序

--- sort.cpp
+++ sort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
@@ -37,7 +38,7 @@ public:
 	}
 
 	//插入排序
-	void insert_sort(vector<int> nums) {
+	void insert_sort(vector<int>& nums) {
 		int n = nums.size();
 		for (int i = 1; i < n; i++) {
 			int pos = i - 1;
@@ -51,27 +52,79 @@ public:
 	}
 
 	//快速排序
-	void fast_sort(vector<int> nums, int start, int end) {
+	void fast_sort(vector<int>& nums, int start, int end) {
 		int n = nums.size();
 		int left = start;
 		int right = end;
 		int temp = nums[left];
 		while (left < right) {
-			while (left<right && nums[right]>nums[left]) {
+			while (left<right && nums[right]>temp) {
 				right--;
-				nums[left] = nums[right];
 			}
-			while (left < right && nums[left] < nums[right]) {
+			nums[left] = nums[right];
+			while (left < right && nums[left] < temp) {
 				left++;
-				nums[right] = nums[left];
 			}
+			nums[right] = nums[left];
 		}
 		nums[left] = temp;
 		fast_sort(nums, start, left - 1);
 		fast_sort(nums, left + 1, end);
 	}
 
+	//归并排序
+	void merge_sort(vector<int>& nums, int left, int right) {
+		//递归划分
+		if (left < right) {
+			int mid = left + (right - left) / 2;
+			//划分左右区间
+			merge_sort(nums, left, mid);
+			merge_sort(nums, mid + 1, right);
+			//合并划分区间
+			merge(nums, left, right);
+		}
+		return;
+	}
+	//归并排序合并函数
+	void merge(vector<int>& nums, int left, int right) {
+		vector<int> temp(nums.size());//临时记录数组
+		int mid = left + (right - left) / 2;
+		int l = left;//左边未排序第一个元素
+		int r = mid + 1;//右边未排序第一个元素
+		int k = left;//临时记录数组的下标
+		while (l <= mid && r <= right) {
+			if (nums[l] < nums[r]) {
+				temp[k++] = nums[l++];
+			}
+			else temp[k++] = nums[r++];
+		}
+		while (l <= mid)temp[k++] = nums[l++];
+		while (r <= right)temp[k++] = nums[r++];
+
+		//拷贝回原数组
+		while (left <= right) {
+			nums[left] = temp[left];
+			left++;
+		}
+	}
+
+
+	//堆排序
+	void stack_sort(vector<int>& nums) {
+		int a = nums.size();
+		priority_queue<int, vector<int>, greater<int>> q;
+		for (int i = 0; i < a; i++) {
+			q.push(nums[i]);
+		}
+		for (int i = 0; i < a; i++) {
+			nums[i] = q.top();
+			q.pop();
+		}
+	}
+
 };
+
+
 
 
 
@@ -82,7 +135,9 @@ int main() {
 		cout << " " << num;
 	}
 	cout << "排序前" << endl;
-	s.buddle_sort(nums);
+	//s.buddle_sort(nums);
+	//s.merge_sort(nums, 0, nums.size() - 1);
+	s.stack_sort(nums);
 	for (int num : nums) {
 		cout << " " << num;
 	}
